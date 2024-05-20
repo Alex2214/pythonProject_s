a887819b0f2dfd63d2073c8e75c6bf05020825a1
Alex2214 <mengoalex@yandex.ru>
2024-05-20 23:50:23 +0200

    work

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Trading App"
+)
+
+
+fake_users = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 1, "role": "investor", "name": "John"},
+    {"id": 1, "role": "trader", "name": "Matt"},
+]
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return [user for user in fake_users if user.get("id") == user_id]
